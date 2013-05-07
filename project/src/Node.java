3f75347a802b7509e0c293d7e8f87da9dd09e152
michael.vuong <mvuo803@cse.unsw.edu.au>
2013-05-07 15:41:02 +1000

    added in Node constants, getters and setters

--- project/src/Node.java
+++ project/src/Node.java
@@ -1,4 +1,54 @@
+import java.util.LinkedList;
 
+/* Node class */
 public class Node {
 
+	/* Class Constants defined here */
+	private static int number;
+	private static LinkedList<Node> box;
+	private static LinkedList<Node> column;
+	private static LinkedList<Node> row;
+	
+	/* Getters */
+	public int getNumber() {
+		
+		return number;
+	}
+	
+	public LinkedList<Node> getRow() { 
+		
+		return row;
+	}
+	
+	public LinkedList<Node> getColumn() { 
+		
+		return column;
+	}
+	
+	public LinkedList<Node> getBox() { 
+		
+		return box;
+	}
+		
+	/* Setters */
+	public static void setNumber(int number) {
+		
+		Node.number = number;
+	}
+
+	public static void setBox(LinkedList<Node> box) {
+		
+		Node.box = box;
+	}
+
+	public static void setColumn(LinkedList<Node> column) {
+		
+		Node.column = column;
+	}
+
+	public static void setRow(LinkedList<Node> row) {
+		
+		Node.row = row;
+	}
+	
 }
