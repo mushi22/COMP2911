98bfee73ae049eab32c2558b5f72c9cebf71f9f1
Daniel Chen <interstellarechoess@gmail.com>
2013-05-16 13:40:49 +1000

    continued graph colouring
    added actual vertex object

---
+++ project/src/Vertex.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * @author
+ */
+public class Vertex {
+	private int value;
+	
+	public Vertex (int value)
+	{
+		this.value = value;
+	}
+	
+	public int getValue()
+	{
+		return value;
+	}
+	
+	
+}
