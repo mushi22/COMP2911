80ed046a74d1d8a440129b2948a490d5dc63cc81
michael.vuong <mvuo803@cse.unsw.edu.au>
2013-05-29 21:39:58 +1000

    moved sudokuBoard

---
+++ project/oldStuff/SudokuBoard.java
@@ -0,0 +1,37 @@
+
+/* This class defines a plain sudoku board for values to be added */
+public class SudokuBoard {
+
+	/* Constants defined here for clarity */
+	private int board[][];		// this represents the 9x9 board
+	private static final int MAX_NUM_COLUMNS = 8;
+	private static final int MAX_NUM_ROWS = 8;
+	
+	/* Constructor */
+	public SudokuBoard () { 
+		
+		// initialising the board
+		this.board = new int[8][8];
+		
+	}
+	
+	public void addValueToNewBoard (int valueToAdd) { 
+		
+		for (int i = 0; i < MAX_NUM_ROWS; i++) {
+			for (int j = 0; j < MAX_NUM_COLUMNS; j++) {
+				board[i][j] = valueToAdd;
+			}
+		}
+	}
+	
+	public void printBoard () {
+		
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j]);
+				System.out.println(" ");
+			}
+		}
+	}
+	
+}
