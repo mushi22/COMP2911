f29a394e252f7292d784ab460f1fca6810dda4fe
Sohaib Mushtaq <msmu890@cse.unsw.edu.au>
2013-06-01 22:56:46 +1000

    new valid function

--- project/src/SolveSudoku.java
+++ project/src/SolveSudoku.java
@@ -9,7 +9,6 @@ public class SolveSudoku {
 	//		 daniel feel free to do some work
    private static final int SMALLBOX_SIZE = 3;
 	boolean[] digits;
-
    
 	//private SudokuBoard sBoard;
 	
@@ -25,22 +24,15 @@ public class SolveSudoku {
       if (i == -1 || j == -1) {
          return sBoard;
       }
-      System.out.print("Modifying cell ("+ i+","+j+")\n");
-      for (Integer k : getPossibilities(i, j, sBoard))
+      for (Integer k : getPossibilities(j, i, sBoard))
       {
-         System.out.print("1\n");
-         sBoard.setCellNum(k.intValue(), i, j);
-         System.out.print("2\n");
+         sBoard.setCellNum(k.intValue(), j, i);
          sBoard.printBoard();
          SudokuBoard temp = copy(sBoard);
-         System.out.print("3\n");
          temp = recursiveBruteForceSolver(temp);
-         System.out.print("4\n");
          if (isLegalBoard(temp) && isComplete(temp)) {
-            System.out.print("5\n");
             return temp;
          }
-         System.out.print("not valid \n");
       } 
 
       return null;
@@ -48,15 +40,15 @@ public class SolveSudoku {
    
    private int[] findEmptyCell(SudokuBoard sBoard)
    {
+      //int[] emptyCell = new int[2];
       int[] emptyCell = {-1, -1};
-      for (int j = 0; j < 9; j++)
+      for (int i = 0; i < 9; i++)
       {
-         for (int i = 0; i < 9; i++)
+         for (int j = 0; i < 9; i++)
          {
-            if (sBoard.getBoard()[i][j]==0) {
+            if (sBoard.getBoard()[j][i]==0) {
                emptyCell[0] = i;
                emptyCell[1] = j;
-               return emptyCell;
             }
          }
       }
@@ -96,103 +88,150 @@ public class SolveSudoku {
       return numbers;
    }
    
-   /**
-    * main function checks board is valid
-    * @return
-    */
-   private boolean isLegalBoard(SudokuBoard sBoard){
-      //if (sBoard == null) return false;
-      for(int i = 0; i <9; i++ ){
-    	  if(!rowValid(i, sBoard)){
-    		  return false;
-    	  }
-      }
-      
-      for(int i = 0; i < 9; i++){
-    	  if(!columnValid(i, sBoard)){
-    		  return false;
-    	  }
-      }
-      
-      for(int i =0; i < 9; i++){
-    	  for(int j = 0; j < 9; j += 3){
-    		  if(!smallBoxValid(i, j, sBoard)){
-    			  return false;
-    		  }
-    	  }
-      }
-      return true;
-   }
    
-   /**
-    * checks a row is valid
-    * @param row
-    * @return
-    */
-   public boolean rowValid (int row, SudokuBoard sBoard){
-	   resetnumbers();
-	   for( int k =0; k <9; k++){
-
-		   if(!numbersManager(sBoard.getBoard()[row][k])){
-			   return false;
-		   }
-	   }
-	   return true;
-   }
+   //public static void Validate(final int [][] sudokuBoard)
    
-   /**
-    * checks a column is valid
-    * @param column
-    * @return
-    */
-   public boolean columnValid(int column, SudokuBoard sBoard){
-	   resetnumbers();
-	   for( int k =0; k <9; k++){
-		   if(!numbersManager( sBoard.getBoard()[column][k])){
-			   return false;
-		   }
-	   }
-	   return true;
-   }
-   
-   /**
-    * checks the smallboxes in sudoku are valid
-    * @param row
-    * @param column
-    * @return
-    */
-   public boolean smallBoxValid(int row, int column, SudokuBoard sBoard){
-	   resetnumbers();
+   public static void Valiate(SudokuBoard sBoard){
 	   
-	   for(int k =0; k < 3;  k++){
-		   for(int c = 0; c <3; c++){
-			   if(!numbersManager(sBoard.getBoard()[k + row][c + column])){
+	   
+	   for(int i = 0; i < 9; i ++){
+		   if(!isValidRow(sBoard, i, 9)){
+			   //row has repitions
+			   }
+	   }   
+	   for(int j = 0; j < 9; j++){
+		   if(!isValidColumn(sBoard, j, 9)){
+			   // columns has repitions
+		   }
+	   }   
+	   
+   }
+   
+   private static boolean isValidColumn(SudokuBoard sBoard, int column, int height){
+	   for(int i = 0; i < 9; i++){
+		   for(int j = i + 1; j < 9; j++){
+			   if(sBoard.getBoard()[i][column] == sBoard.getBoard()[j][column]){
 				   return false;
 			   }
 		   }
 	   }
 	   return true;
    }
-   /**
-    * resets numbers to false
-    */
-   public void resetnumbers(){
-	   digits = new boolean[10];
+   
+   private static boolean isValidRow(SudokuBoard sBoard, int row, int width){
+	   for(int i = 0; i < 9; i++){
+		   for(int j = i + 1; j < 9; j++){
+			   if(sBoard.getBoard()[row][i] == sBoard.getBoard()[row][i]){
+				   return false;
+			   }
+		   }
+	   }
+	   return true;
    }
    
-  /**
-   * keeps track of numbers used in Sudoku
-   * @param number
-   * @return
-   */
-   public boolean numbersManager(int number){
-	   if( number != 0 && digits[number]){
-		   return false;
-	   }else{
-		   digits[number] = true;
-		   return true;
-	   }
-   }
+   /**
+    * main function checks board is valid
+    * @return
+    */
+//   private boolean isLegalBoard(SudokuBoard sBoard){
+//      //if (sBoard == null) return false;
+//      for(int i = 0; i <9; i++ ){
+//    	  if(!rowValid(i, sBoard)){
+//    		  return false;
+//    	  }
+//      }
+//      
+//      for(int i = 0; i < 9; i++){
+//    	  if(!columnValid(i, sBoard)){
+//    		  return false;
+//    	  }
+//      }
+//      
+//      for(int i =0; i < 9; i++){
+//    	  for(int j = 0; j < 9; j += 3){
+//    		  if(!smallBoxValid(i, j, sBoard)){
+//    			  return false;
+//    		  }
+//    	  }
+//      }
+//      return true;
+//   }
+//   
+//   /**
+//    * checks a row is valid
+//    * @param row
+//    * @return
+//    */
+//   public boolean rowValid (int row, SudokuBoard sBoard){
+//	   resetnumbers();
+//	   for( int k =0; k <9; k++){
+//
+//		   if(!numbersManager(sBoard.getBoard()[row][k])){
+//			   return false;
+//		   }
+//	   }
+//	   return true;
+//   }
+//   
+//   /**
+//    * checks a column is valid
+//    * @param column
+//    * @return
+//    */
+//   // comments : numbersMANAGER (INT) _> (INT [][] 
+//  
+//   //find the location of col in the boad
+//   //basically you need ot test if col 
+//   //numbersMNAGER 
+//   
+//   public boolean columnValid(int column, SudokuBoard sBoard){
+//	   resetnumbers();
+//	     for( int k =0; k <9; k++){
+//		   
+//		   
+//		   
+//	   }
+//	   return true;
+//   }
+//   
+//   /**
+//    * checks the smallboxes in sudoku are valid
+//    * @param row
+//    * @param column
+//    * @return
+//    */
+//   public boolean smallBoxValid(int row, int column, SudokuBoard sBoard){
+//	   resetnumbers();
+//	   
+//	   for(int k =0; k < 3;  k++){
+//		   for(int c = 0; c <3; c++){
+//			   if(!numbersManager(sBoard.getBoard()[k + row][c + column])){
+//				   return false;
+//			   }
+//		   }
+//	   }
+//	   return true;
+//   }
+//   /**
+//    * resets numbers to false
+//    */
+//   public void resetnumbers(){
+//	   digits = new boolean[10];
+//   }
+//   
+//  /**
+//   * keeps track of numbers used in Sudoku
+//   * @param number
+//   * @return
+//   */
+//   public boolean numbersManager(int number){
+//	   if( number != 0 && digits[number]){
+//		   return false;
+//	   }else{
+//		   digits[number] = true;
+//		   return true;
+//	   }
+//   }
    
    
    
